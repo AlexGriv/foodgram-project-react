5f9f690ab7f61f92a86fd142666f2fd7813423d6
AlexGriv <lksa-wd@yandex.ru>
2022-08-21 22:32:30 +1000

    first6

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 
 from .filters import IngredientFilter, RecipeFilter
